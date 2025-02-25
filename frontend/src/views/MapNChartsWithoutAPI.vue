34225c4907ae5254385b9c87c338a8ac67183e46
instentbanjo <nagyshirofa@gmail.com>
2025-02-25 15:59:07 +0000

    buttons styled

--- frontend/src/views/MapNChartsWithoutAPI.vue
+++ frontend/src/views/MapNChartsWithoutAPI.vue
@@ -405,12 +405,12 @@ onMounted(async () => {
             <div style="display: flex; justify-content: center; align-items: center; width: 100%; position: relative;">
               <!-- Centered Section -->
               <div style="display: flex; flex-direction: column; align-items: center;">
-                <div>
+                <div style="display: flex">
                   <label for="f-option" class="l-radio">
-                    <button class="tab-button" @click="heatSelected = true">Temperature</button>
+                    <button class="button-4" @click="heatSelected = true">Temperature</button>
                   </label>
                   <label for="s-option" class="l-radio">
-                    <button class="tab-button" @click="heatSelected = false">Wind</button>
+                    <button class="button-4" @click="heatSelected = false">Wind</button>
                   </label>
                 </div>
                 <div>
@@ -731,5 +731,62 @@ onMounted(async () => {
 }
 
 
+.button-4 {
+  appearance: none;
+  background-color: #FAFBFC;
+  border: 1px solid rgba(27, 31, 35, 0.15);
+  border-radius: 6px;
+  box-shadow: rgba(27, 31, 35, 0.04) 0 1px 0, rgba(255, 255, 255, 0.25) 0 1px 0 inset;
+  box-sizing: border-box;
+  color: #24292E;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system, system-ui, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji";
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  list-style: none;
+  padding: 6px 16px;
+  margin: 6px;
+  position: relative;
+  transition: background-color 0.2s cubic-bezier(0.3, 0, 0.5, 1);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+  white-space: nowrap;
+  word-wrap: break-word;
+}
+
+.button-4:hover {
+  background-color: #F3F4F6;
+  text-decoration: none;
+  transition-duration: 0.1s;
+}
+
+.button-4:disabled {
+  background-color: #FAFBFC;
+  border-color: rgba(27, 31, 35, 0.15);
+  color: #959DA5;
+  cursor: default;
+}
+
+.button-4:active {
+  background-color: #EDEFF2;
+  box-shadow: rgba(225, 228, 232, 0.2) 0 1px 0 inset;
+  transition: none 0s;
+}
+
+.button-4:focus {
+  outline: 1px transparent;
+}
+
+.button-4:before {
+  display: none;
+}
+
+.button-4:-webkit-details-marker {
+  display: none;
+}
 </style>
 
